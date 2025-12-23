8fd88ea7a7e8933531066d70db6c61ce2c4acee8
ChaLyn03 <charleslynchj@outlook.com>
2025-12-23 15:24:36 +1030

    -

--- test.py
+++ test.py
@@ -1,19 +1,38 @@
 from mido import Message, MidiFile, MidiTrack
+import random
 
 mid = MidiFile(ticks_per_beat=480)
-track = MidiTrack()
-mid.tracks.append(track)
 
-# Set instrument to Acoustic Grand Piano
-track.append(Message('program_change', program=0, time=0))
+voices = []
+for _ in range(4):
+    track = MidiTrack()
+    mid.tracks.append(track)
+    track.append(Message('program_change', program=0, time=0))
+    voices.append(track)
 
-# Simple melody (C major)
-notes = [
-    60, 62, 64, 65, 67, 65, 64, 62, 60
+# Pitch materials (intentionally clashing)
+scales = [
+    [60, 62, 63, 65, 67, 68, 71],      # C minor-ish
+    [63, 65, 67, 68, 70, 72, 74],      # Eb lydian-ish
+    [55, 57, 59, 60, 62, 64, 66],      # G chromatic cluster
+    [48, 50, 51, 53, 55, 56, 59],      # Low-register instability
 ]
 
-for note in notes:
-    track.append(Message('note_on', note=note, velocity=64, time=0))
-    track.append(Message('note_off', note=note, velocity=64, time=480))
+durations = [120, 240, 360, 480]  # irregular rhythmic grid
 
-mid.save('simple_melody.mid')
+bars = 64
+notes_per_bar = 6
+
+for bar in range(bars):
+    for v, track in enumerate(voices):
+        scale = random.choice(scales)
+        register_shift = random.choice([-12, 0, 12])
+
+        for _ in range(notes_per_bar):
+            note = random.choice(scale) + register_shift
+            dur = random.choice(durations)
+
+            track.append(Message('note_on', note=note, velocity=50, time=0))
+            track.append(Message('note_off', note=note, velocity=50, time=dur))
+
+mid.save("overcomplex_experiment.mid")
